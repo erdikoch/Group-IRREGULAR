2be2b1f8e0205d0ecc98d257bdcd932f8dccfa04
erdikoch <erdi.koc@ozu.edu.tr>
2014-04-12 13:21:47 +0300

    .java docs added !

---
+++ CrazyCopterBattle/src/Game.java
@@ -0,0 +1,18 @@
+/*
+ * Cs 320 Group Project / Group IRREGULAR
+ * 
+ * Crazy Copter Java Application Game Project (CCB)
+ * 
+ * Group Members : 
+ * 1 - Erdi Koç
+ * 2 - Gamze Küçükçolak
+ * 3 - Mehmet Kaðan Kayaalp
+ * 4 - Nazlý Karalar
+ * 5 - Ýsmetcan Hergünþen
+ * 
+ */
+
+
+public class Game {
+
+}
