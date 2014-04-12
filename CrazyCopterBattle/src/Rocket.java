c10631123a8f8b848aa9d46f1f565f1ff3309653
nazlikaralar <nazli.karalar@ozu.edu.tr>
2014-04-12 20:53:41 +0300

    rocket

--- CrazyCopterBattle/src/Rocket.java
+++ CrazyCopterBattle/src/Rocket.java
@@ -12,7 +12,21 @@
  * 
  */
 
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
 public class Rocket {
+	public int xCoordinate;
+	public int yCoordinate;
+	private double movingXspeed;
 
+	public void Initialize(int xCoordinate, int yCoordinate) {
+		setCoordinates(xCoordinate, yCoordinate);
+		this.movingXspeed = 23;
+	}
+
+	private void setCoordinates(int xCoordinate, int yCoordinate) {
+		this.xCoordinate = xCoordinate;
+		this.yCoordinate = yCoordinate;
+	}
 }
