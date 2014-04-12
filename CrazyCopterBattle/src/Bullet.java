a6979ba541e04bb9daa8c5513c26b4568189ec42
erdikoch <erdi.koc@ozu.edu.tr>
2014-04-12 20:26:37 +0300

    Bullet Constructor added and initialized !

--- CrazyCopterBattle/src/Bullet.java
+++ CrazyCopterBattle/src/Bullet.java
@@ -12,6 +12,43 @@
  * 
  */
 
+import java.awt.Point;
+
 public class Bullet {
+
+     public double xCoordinate;
+	public double yCoordinate;
+	private static int bulletSpeed = 20;
+	private double movingXspeed;
+	private double movingYspeed;
+
+public Bullet(int xCoordinate, int yCoordinate, Point mousePosition) {
+		this.xCoordinate = xCoordinate;
+		this.yCoordinate = yCoordinate;
+		setDirectionAndSpeed(mousePosition);
+
+	}
+
+	private void setDirectionAndSpeed(Point mousePosition) {
+		// Unit direction vector of the bullet.
+		double directionVx = mousePosition.x - this.xCoordinate;
+		double directionVy = mousePosition.y - this.yCoordinate;
+		double lengthOfVector = Math.sqrt(directionVx * directionVx
+				+ directionVy * directionVy);
+		
+		directionVx = directionVx / lengthOfVector;
+		directionVy = directionVy / lengthOfVector;
+
+		setSpeeds(directionVx, directionVy);
+	}
+
+	private void setSpeeds(double directionVx, double directionVy) {
+		this.movingXspeed = bulletSpeed * directionVx;
+		this.movingYspeed = bulletSpeed * directionVy;
+	}
+
+
+
+
 
 }
