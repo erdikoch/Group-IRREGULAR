f7c5979c1fc1d12870121dcca3c9762476221e8d
hergunsen <ismetcan.hergunsen@ozu.edu.tr>
2014-04-14 20:53:11 +0300

    controller

--- CrazyCopterBattle/src/UserController.java
+++ CrazyCopterBattle/src/UserController.java
@@ -11,14 +11,17 @@
  * 5 - Ýsmetcan Hergünþen
  * 
  */
+
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import javax.swing.JPanel;
 
-
-public abstract class UserController implements KeyListener, MouseListener {
+public abstract class UserController extends JPanel implements KeyListener,
+		MouseListener {
 
 	private static final int NUM_OF_KEYBOARD_FUNCTIONS = 525;
 	private static final int NUM_OF_MOUSE_FUNCTIONS = 3;
@@ -27,8 +30,40 @@ public abstract class UserController implements KeyListener, MouseListener {
 	private static boolean[] mouseState = new boolean[NUM_OF_MOUSE_FUNCTIONS];
 
 	public UserController() {
+
+		this.setDoubleBuffered(true);
+		this.setFocusable(true);
+		this.setBackground(Color.black);
+
+		// If you will draw your own mouse cursor or if you just want that mouse
+		// cursor disappear,
+		// insert "true" into if condition and mouse cursor will be
+		// removed.(taken from " www.stackoverflow.com ")
+		if (true) {
+			BufferedImage blankCursorImg = new BufferedImage(16, 16,
+					BufferedImage.TYPE_INT_ARGB);
+			Cursor blankCursor = Toolkit.getDefaultToolkit()
+					.createCustomCursor(blankCursorImg, new Point(0, 0), null);
+			this.setCursor(blankCursor);
+		}
+
+		this.addKeyListener(this);
+		this.addMouseListener(this);
 	}
-	
+
+	public abstract void Draw(Graphics2D g2d);
+
+	@Override
+	public void paintComponent(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		super.paintComponent(g2d);
+		Draw(g2d);
+	}
+
+	public static boolean keyboardKeyState(int key) {
+		return keyboardState[key];
+	}
+
 	@Override
 	public void keyPressed(KeyEvent e) {
 		keyboardState[e.getKeyCode()] = true;
@@ -83,4 +118,3 @@ public abstract class UserController implements KeyListener, MouseListener {
 	}
 
 }
-
