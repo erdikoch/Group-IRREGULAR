d44b0c0e9d2055b42e97ecc0294eaca82b2793a3
gamzekucukcolak <gamze.kucukcolak@ozu.edu.tr>
2014-04-15 21:17:56 +0300

    Game part is commited

--- CrazyCopterBattle/src/Game.java
+++ CrazyCopterBattle/src/Game.java
@@ -15,4 +15,73 @@
 
 public class Game {
 
+	public Game() {
+		loadGameContent();
+		Thread threadForInitGame = new Thread() {
+			@Override
+			public void run() {
+				initialize();
+				LoadContent();
+				playGame();
+			}
+
+			private void playGame() {
+				Framework.gameState = Framework.GameState.PLAYING;
+			}
+		};
+		threadForInitGame.start();
+	}
+
+	private void loadGameContent() {
+		Framework.gameState = Framework.GameState.GAME_CONTENT_LOADING;
+	}
+
+	private void initialize() {
+
+	}
+
+	private void LoadContent() {
+		loadBackgroundImages();
+		loadEnemyImages();
+		loadRocketImages();
+		loadRocketSmokeImages();
+		loadMouseCursorImage();
+		loadBulletImage();
+		loadExplosionImages();
+	}
+
+	private void loadBackgroundImages() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void loadEnemyImages() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void loadRocketImages() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void loadRocketSmokeImages() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void loadMouseCursorImage() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void loadBulletImage() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void loadExplosionImages() {
+		// TODO Auto-generated method stub
+
+	}
 }
