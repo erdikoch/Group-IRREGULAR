c38dffa852ae402a508d61a9211309e57e04a5a1
gamzekucukcolak <gamze.kucukcolak@ozu.edu.tr>
2014-04-15 16:55:58 +0300

    RocketSmoke is done

--- CrazyCopterBattle/src/RocketSmoke.java
+++ CrazyCopterBattle/src/RocketSmoke.java
@@ -12,7 +12,7 @@
  * 5 - Ýsmetcan Hergünþen
  * 
  */
-
+import java.awt.AlphaComposite;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
@@ -45,7 +45,7 @@ public class RocketSmoke {
 
 
 
-	public boolean didSmokeDisapper(long gameTime){
+	public boolean isSmokeDisapper(long gameTime){
 		long currentLifeTime = gameTime - timeOfCreation;
 
 		if(currentLifeTime >= smokeLifeTime)
@@ -53,10 +53,36 @@ public class RocketSmoke {
 		else
 			return false;
 	}
+	public void updateTransparency(long gameTime) {
+
+		long currentLifeTime = gameTime - timeOfCreation;
+
+		int currentLTInPercentages = (int) (currentLifeTime * 100 / smokeLifeTime);
+		currentLTInPercentages = 100 - currentLTInPercentages;
+		float rSmokeTransparency = 1.0f * (currentLTInPercentages * 0.01f);
+
+		if (rSmokeTransparency > 0)
+			imageTransparency = rSmokeTransparency;
+	}
 
 
-	public void Draw(Graphics2D g2d)
-	{
+	public void Draw(Graphics2D g2d){
+	g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER,
+				imageTransparency));
+
+		// While smoke is disappearing (imageTransparency), it also expanding.
+		// We will multiply smoke image with imageMultiplier so that will slowly
+		// become bigger.
+		float imageMultiplier = 2 - imageTransparency;
+		int newImageWidth = (int) (smokeImg.getWidth() * imageMultiplier);
+		int newImageHeight = (int) (smokeImg.getHeight() * imageMultiplier);
+		// We will set new y coordinate of smoke so that its stays in center
+		// behind the rocket.
+		int newImageYCoordinate = (int) (smokeImg.getHeight() / 2 * (1 - imageTransparency));
+		g2d.drawImage(smokeImg, xCoordinate, yCoordinate - newImageYCoordinate,
+				newImageWidth, newImageHeight, null);
+
+		g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER));
 	}
 
 }
