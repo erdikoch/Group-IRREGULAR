051d4c4a67d5b5a42f0d12c709780cf2f0844ea8
Mehmet Kayaalp <mehmet.kayaalp@ozu.edu.tr>
2014-04-15 23:18:24 +0300

    Rocket

--- CrazyCopterBattle/src/Rocket.java
+++ CrazyCopterBattle/src/Rocket.java
@@ -12,18 +12,48 @@
  * 
  */
 
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
 public class Rocket {
+
+	public final static long timeBetweenNewRockets = Framework.secInNanosec / 4;
+	public static long timeOfLastCreatedRocket = 0;
+
+	public static int damagePower = 100;
+
 	public int xCoordinate;
 	public int yCoordinate;
+
 	private double movingXspeed;
+	public long currentSmokeLifeTime;
+	public static BufferedImage rocketImg;
 
 	public void Initialize(int xCoordinate, int yCoordinate) {
 		setCoordinates(xCoordinate, yCoordinate);
 		this.movingXspeed = 23;
+		this.currentSmokeLifeTime = Framework.secInNanosec / 2;
 	}
 
 	private void setCoordinates(int xCoordinate, int yCoordinate) {
 		this.xCoordinate = xCoordinate;
 		this.yCoordinate = yCoordinate;
 	}
+
+	public boolean isLeftScreen() {
+		if (xCoordinate > 0 && xCoordinate < Framework.frameWidth)
+			return false;
+		else
+			return true;
+	}
+
+	public void Update() {
+		xCoordinate += movingXspeed;
+	}
+
+	public void Draw(Graphics2D g2d) {
+		g2d.drawImage(rocketImg, xCoordinate, yCoordinate, null);
+	}
 }
+
+
