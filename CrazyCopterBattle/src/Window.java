dd8ef0386cfe41ac69a4b4f777dcf003749cf3a9
hergunsen <ismetcan.hergunsen@ozu.edu.tr>
2014-04-15 20:34:44 +0300

    windows

--- CrazyCopterBattle/src/Window.java
+++ CrazyCopterBattle/src/Window.java
@@ -12,6 +12,35 @@
  * 
  */
 
-public class Window {
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
+public class Window extends JFrame {
+
+	private Window() {
+		this.setTitle("Crazy Copter Battle");
+
+		if (true) {
+			this.setUndecorated(true);
+			this.setExtendedState(this.MAXIMIZED_BOTH);
+		} else {
+			this.setSize(1024, 768);
+			this.setLocationRelativeTo(null);
+			this.setResizable(false);
+		}
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		this.setContentPane(new Framework());
+		this.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new Window();
+			}
+		});
+	}
 }
